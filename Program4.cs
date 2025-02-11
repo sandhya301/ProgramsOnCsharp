199f1b98f992f1ff4a54a7d21ccdf74f368301e5
Sandhya <sandhyagh20cl@gmail.com>
2025-02-11 17:29:53 +0530

    Program4 added

---
+++ Program4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    class Program4
+    {
+        public static async Task ProcessOrder()
+        {
+            Console.WriteLine("Enter your order name");
+            string orderName = Console.ReadLine();
+            Console.WriteLine("Processing order...");
+            await Task.Delay(3000);
+            Console.WriteLine($"Your order for {orderName} is ready!!");
+        }
+        
+        public static async Task Main(string[] args)
+        {
+            await ProcessOrder();
+
+        }
+    }
+
+}
