03de79030af1f8eb935538c90ca8dd46d640e701
Sandhya <sandhyagh20cl@gmail.com>
2025-02-11 16:30:49 +0530

    Program1 added

--- Program1.cs
+++ Program1.cs
@@ -1,8 +1,53 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
-public class Class1
+
+namespace Programs
 {
-	public Class1()
-	{
-	}
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\PGYSW023\Desktop\Programs\output.txt";
+            Console.WriteLine("Enter multiple lines of text.Type STOP to end.");
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                while (true)
+                {
+                    string inputText = Console.ReadLine();
+                    if (inputText.Contains("STOP"))
+                    {
+
+                        break;
+                    }
+
+                    sb.AppendLine(inputText);
+
+
+                }
+                File.WriteAllText(path, sb.ToString());
+                string readText = File.ReadAllText(path);
+                Console.WriteLine("File Content:");
+                Console.WriteLine(readText);
+
+
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Error:Access to your file is denied.Please check your file permission");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("Error: The Directory your looking for is not found.Please Check the Path.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+    }
 }
