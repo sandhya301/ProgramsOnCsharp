facc80d666bb20226914ac7ad4c2ee112e4db913
Sandhya <sandhyagh20cl@gmail.com>
2025-02-12 11:25:04 +0530

    Program1 modified

--- Program1.cs
+++ Program1.cs
@@ -8,6 +8,7 @@ namespace Programs
     class Program1
     {
         static void Main(string[] args)
+
         {
             string path = @"C:\Users\PGYSW023\Desktop\Programs\output.txt";
             Console.WriteLine("Enter multiple lines of text.Type STOP to end.");
@@ -24,31 +25,29 @@ namespace Programs
                     }
 
                     sb.AppendLine(inputText);
-
-
                 }
+
                 File.WriteAllText(path, sb.ToString());
                 string readText = File.ReadAllText(path);
                 Console.WriteLine("\n");
                 Console.WriteLine("File Content:\n");
                 Console.WriteLine(readText);
-
-
             }
+
             catch (UnauthorizedAccessException e)
             {
                 Console.WriteLine("Error:Access to your file is denied.Please check your file permission");
             }
+
             catch (DirectoryNotFoundException e)
             {
                 Console.WriteLine("Error: The Directory your looking for is not found.Please Check the Path.");
             }
+
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
         }
-
-
-    }
+     }
 }
